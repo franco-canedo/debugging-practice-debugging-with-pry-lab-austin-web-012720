c78414381e41a38b09b37cfed6787618d3aceda4
Franco Canedo <francanedo1@hotmail.com>
2020-01-23 18:44:04 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,11 +13,11 @@ end
 def selection(num)
   if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  end
- if num == 2
+  
+ else if num == 2
      "HAM HAM HAM IN MY TUMMY"
   end
-
+end 
 end
 
 def runner
